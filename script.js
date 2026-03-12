73b2a3ccfc822a2dd2027430f8700ad4bb5b9dfe
melisbm <miquelangel.melis@gmail.com>
2026-03-12 19:41:06 +0100

    ADD responsiveness

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-const cityOrigin = document.querySelector('input[name="city-origin"]');
+const originCityInput = document.querySelector('input[name="origin-city"]');
+const destinationCityInput = document.querySelector('input[name="destination-city"]');
+const xpInput = document.querySelector('input[name="xp"]');
+const goldInput = document.querySelector('input[name="gold"]');
+const destinationPersonInput = document.querySelector('input[name="destination-person"]');
+
+document.querySelector("form").addEventListener("submit", function(event){
+
+    event.preventDefault();
+    console.log(originCityInput.value);
+});
